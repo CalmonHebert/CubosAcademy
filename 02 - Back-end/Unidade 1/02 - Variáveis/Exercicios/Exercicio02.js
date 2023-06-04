9a1277ca5e4a04a32c9b4e5fed37cf684c47453a
CalmonHebert <102420193+CalmonHebert@users.noreply.github.com>
2023-06-04 19:47:16 -0300

    Exercicio 2

---
+++ 02 - Back-end/Unidade 1/02 - Variáveis/Exercicios/Exercicio02.js
@@ -0,0 +1,9 @@
+// Declare uma variável mutável chamada populacao, guarde a população da sua cidade nessa variavel.
+let populacao = 178000;
+//Imprima essa variavel na tela
+console.log(populacao);
+// Altere o conteudo dessa variavel para um outro número qualquer
+populacao = 200_000;
+//Imprima novamente a variavel na tela
+console.log(populacao);
+
