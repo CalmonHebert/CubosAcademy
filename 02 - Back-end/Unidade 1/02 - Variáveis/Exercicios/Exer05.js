f33014ef3ba9b0198fb1a79e63b549c30e39631b
CalmonHebert <102420193+CalmonHebert@users.noreply.github.com>
2023-06-05 09:29:26 -0300

    exericio 5

---
+++ 02 - Back-end/Unidade 1/02 - Variáveis/Exercicios/Exer05.js
@@ -0,0 +1,16 @@
+let lado = 5;
+let largura = 16;
+let comprimento = 6;
+let base = 12;
+let altura = 8;
+let resposta = 0;
+
+// quadrado lado * lado
+resposta = lado * lado;
+console.log(`A área do quadrado é ${resposta}`);
+// retangulo = largura * comprimento
+resposta = largura * comprimento;
+console.log(`A área do retangulo é ${resposta}`);
+//triangulo = base * altura / 2
+resposta = (base * altura) / 2;
+console.log(`A área do triangulo é ${resposta}`);
