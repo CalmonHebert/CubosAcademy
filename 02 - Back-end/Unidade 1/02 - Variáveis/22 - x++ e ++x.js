f36df860409ea46e4e8c5c3f8bd88f520e93051d
CalmonHebert <102420193+CalmonHebert@users.noreply.github.com>
2023-06-05 09:19:20 -0300

    x++

---
+++ 02 - Back-end/Unidade 1/02 - Variáveis/22 - x++ e ++x.js
@@ -0,0 +1,19 @@
+let x = 10;
+x++;
+console.log(x);
+
+let y = 10;
+y--;
+console.log(y);
+
+//x++ = retorna depois soma
+x = 10;
+let z = x++;
+console.log('Z: ' + z);
+console.log('X: ' + x);
+
+//++x = soma antes e retorna
+x = 10;
+let w = ++x;
+console.log('W: ' + w);
+console.log('X: ' + x);
