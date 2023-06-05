b6d41a952319bd4cb27521b51c10229632b0929f
CalmonHebert <102420193+CalmonHebert@users.noreply.github.com>
2023-06-05 07:39:06 -0300

    null e undefined

---
+++ 02 - Back-end/Unidade 1/02 - Variáveis/13 - Exemplificando com null e undefined.js
@@ -0,0 +1,11 @@
+const nome = 'Calmon';
+const idade = 33;
+const altura = 1.65;
+let ehProf = false;
+let nota;
+
+console.log(nota);
+nota = 10;
+console.log(nota);
+nota = null;
+console.log(nota);
