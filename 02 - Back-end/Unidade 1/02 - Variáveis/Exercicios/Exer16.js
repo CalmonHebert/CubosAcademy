8dd978c77790965fde47703dd1f16ceb980117e0
CalmonHebert <102420193+CalmonHebert@users.noreply.github.com>
2023-06-05 16:56:38 -0300

    exercicio volume esfera

---
+++ 02 - Back-end/Unidade 1/02 - Variáveis/Exercicios/Exer16.js
@@ -0,0 +1,6 @@
+// Volume de uma esfera
+const diametro = 6;
+const raio = diametro / 2;
+const volume = 4 / 3 * raio ** 3;
+
+console.log(`O volume de uma esfera de raio ${raio} é ${volume} PI ou ${volume * Math.PI} `);
