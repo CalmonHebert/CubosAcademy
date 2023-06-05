1875d8a2f727d1d09bc8e03c1d416d318b1038c1
CalmonHebert <102420193+CalmonHebert@users.noreply.github.com>
2023-06-05 07:45:58 -0300

    operadores

---
+++ 02 - Back-end/Unidade 1/02 - Variáveis/14 - Operadores aritiméticos.js
@@ -0,0 +1,14 @@
+let x = 10;
+let y = 2;
+
+let resultado = x + y;
+console.log(resultado);
+
+resultado = x - y;
+console.log(resultado);
+
+resultado = x * y;
+console.log(resultado);
+
+resultado = x / y;
+console.log(resultado);
