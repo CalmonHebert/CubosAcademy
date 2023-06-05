215b1a3fc328726f586df63d40ffcad63d308cdb
CalmonHebert <102420193+CalmonHebert@users.noreply.github.com>
2023-06-05 08:40:21 -0300

    NaN

---
+++ 02 - Back-end/Unidade 1/02 - Variáveis/18 - Infitnity e NaN.js
@@ -0,0 +1,9 @@
+let x = 10;
+let y = 0;
+
+resultado = x / y;
+console.log(`A divisão de ${x} e ${y} é ${resultado}`);
+
+let z = 0
+resultado = z / y;
+console.log(`A divisão de ${x} e ${y} é ${resultado}`);
