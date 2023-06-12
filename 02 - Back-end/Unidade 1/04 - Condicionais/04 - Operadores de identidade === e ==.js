81529eea7bfd6248cee9cdaddba99f06fc5abff2
CalmonHebert <102420193+CalmonHebert@users.noreply.github.com>
2023-06-12 14:57:29 -0300

    Create 04 - Operadores de identidade === e ==.js

---
+++ 02 - Back-end/Unidade 1/04 - Condicionais/04 - Operadores de identidade === e ==.js
@@ -0,0 +1,9 @@
+const x = 17;
+const y = '17';
+
+const ehIdentico = x === 17;
+console.log(ehIdentico);
+let ehIdentico2 = y === 17;
+console.log(ehIdentico2);
+const ehDIferente = x !== 17;
+console.log(ehDIferente);
