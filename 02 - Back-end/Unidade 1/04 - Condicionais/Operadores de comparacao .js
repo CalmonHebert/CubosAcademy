d99aa902c49c3e463d04da8ff0118b1dcd72a685
CalmonHebert <102420193+CalmonHebert@users.noreply.github.com>
2023-06-12 07:46:09 -0300

    estudo operadores

---
+++ 02 - Back-end/Unidade 1/04 - Condicionais/Operadores de comparacao .js
@@ -0,0 +1,12 @@
+const x = 16;
+const y = 17;
+const z = 17
+
+const ehMaior = x > y;
+console.log(ehMaior);
+
+const ehMenor = x < y;
+console.log(ehMenor);
+
+const igual = y >= z;
+console.log(igual);
