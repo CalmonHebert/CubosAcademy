c3453f4d2b74b2227cb20d16ac71cbb995b9ce57
CalmonHebert <102420193+CalmonHebert@users.noreply.github.com>
2023-06-13 20:57:27 -0300

    Create Exer01.js

---
+++ 02 - Back-end/Unidade 1/04 - Condicionais/Exercicios/Exer01.js
@@ -0,0 +1,9 @@
+const valorDaCompra = 100;
+const numeroDeParcelas = 3;
+const desconto = 10 / 100;
+
+if (numeroDeParcelas === 1) {
+  console.log(`Pagamento a vista com 10% de desconto, você terá que pagar ${valorDaCompra - (valorDaCompra * desconto)} reais.`);
+} else {
+  console.log(`Você terá que pagar ${valorDaCompra} em ${numeroDeParcelas} parcelas de ${valorDaCompra / numeroDeParcelas}`);
+}
