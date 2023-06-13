bf18752604b2663a1cc332bc60597cfaeb16368d
CalmonHebert <102420193+CalmonHebert@users.noreply.github.com>
2023-06-13 16:52:48 -0300

    Create 15 - Truthy e Falsy.js

---
+++ 02 - Back-end/Unidade 1/04 - Condicionais/15 - Truthy e Falsy.js
@@ -0,0 +1,32 @@
+const x = 17;
+
+if (x) {
+  console.log("Truthy");
+} else {
+  console.log("Falsy");
+}
+
+const w = "17";
+
+if (w) {
+  console.log("Truthy");
+} else {
+  console.log("Falsy");
+}
+//falsy
+const y = 0;
+
+if (y) {
+  console.log("Truthy");
+} else {
+  console.log("Falsy");
+}
+
+const z = "";
+
+if (z) {
+  console.log("Truthy");
+} else {
+  console.log("Falsy");
+}
+
