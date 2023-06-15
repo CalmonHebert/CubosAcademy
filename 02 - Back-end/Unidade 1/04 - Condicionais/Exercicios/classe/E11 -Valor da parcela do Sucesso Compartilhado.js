3b0a0d58dbef6b4b6c7119364deb363b782b7580
CalmonHebert <102420193+CalmonHebert@users.noreply.github.com>
2023-06-15 17:00:21 -0300

    Update E11 -Valor da parcela do Sucesso Compartilhado.js

--- 02 - Back-end/Unidade 1/04 - Condicionais/Exercicios/classe/E11 -Valor da parcela do Sucesso Compartilhado.js
+++ 02 - Back-end/Unidade 1/04 - Condicionais/Exercicios/classe/E11 -Valor da parcela do Sucesso Compartilhado.js
@@ -1,25 +1,18 @@
-const rendaMensal = 3000;
+const rendaMensal = 1700;
 
 // Tempo decorrido de contrato. Se for maior que 60 meses, o aluno não deve mais nada.
-const mesesDecorridos = 12;
+const mesesDecorridos = 10;
 
 // Soma das parcelas já pagas pelo aluno nos meses anteriores. Se for igual a 18 mil reais, o aluno não deve pagar mais nada, pois já quitou a dívida.
 const totalJaPagoPeloAluno = 10000;
 
-
-//Na Cubos Academy alguns estudantes podem fazer o Curso de Programação do Zero e só começar a pagar após finalizado o curso quando (e se) tiverem obtido renda acima de R$ 2.000,00. Nesse caso o valor da parcela a ser paga é de 18% do valor da renda mensal da pessoal.
-
-//Após 60 meses, independente de ter quitado ou não o valor total do curso (de R$ 18.000,00) a pessoa não deve nada, ou seja, o valor da parcela é zero.
-
-//a) Faça um programa que calcula o valor da parcela a ser paga pelo aluno. Imprima uma mensagem bonita na tela, com o valor em reais.
-if (rendaMensal > 2000) {
+if (mesesDecorridos == 60 || totalJaPagoPeloAluno == 18000) {
+  console.log("Não deve nada. Dívida paga.");
+}
+else if (rendaMensal > 2000) {
   const porcent = 18 / 100;
   const calculo = rendaMensal * porcent;
   console.log(`O valor da parcela desse mês é R$ ${calculo} reais`);
 } else {
-  console.log(`O valor da parcela desse mês é R$ 0 reais`);
+  console.log(`O valor da parcela desse mês é R$ 0 reais. Nenhum valor é devido pois a renda do estudante está abaixo do valor mínimo de R$ 2000 reais.`);
 }
-
-
-
-//b) Altere o seu programa anterior para que, quando o valor da parcela a ser pago for 0, a mensagem explique porque o aluno não precisa pagar nada naquele mês.
