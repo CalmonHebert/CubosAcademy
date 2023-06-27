1677584893fb62cbde57f139a2e8487740d5d14c
CalmonHebert <102420193+CalmonHebert@users.noreply.github.com>
2023-06-27 08:02:20 -0300

    Create 06 - push() e pop() - inserindo e removendo elementos ao fim de um array.js

---
+++ 02 - Back-end/Unidade 1/05 - Arrays e Loops/06 - push() e pop() - inserindo e removendo elementos ao fim de um array.js
@@ -0,0 +1,6 @@
+const nomes = ['Jose', 'Maria', 'Pedro', 'Joana'];
+
+nomes.push('Mais um nome'); //-> .push adiciona um item ao final do array
+console.log(nomes);
+nomes.pop(); //-> .pop retira um item do final do array
+console.log(nomes);
