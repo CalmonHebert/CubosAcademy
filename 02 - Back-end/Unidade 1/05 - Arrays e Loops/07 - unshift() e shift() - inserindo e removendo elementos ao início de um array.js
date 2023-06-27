483d209063d95ed5fb8ee023538cb10b6d1f6f96
CalmonHebert <102420193+CalmonHebert@users.noreply.github.com>
2023-06-27 08:06:50 -0300

    Create 07 - unshift() e shift() - inserindo e removendo elementos ao início de um array.js

---
+++ 02 - Back-end/Unidade 1/05 - Arrays e Loops/07 - unshift() e shift() - inserindo e removendo elementos ao início de um array.js
@@ -0,0 +1,6 @@
+const nomes = ['Jose', 'Maria', 'Pedro', 'Joana'];
+
+nomes.shift(); // -> retira o primeiro item do array.
+console.log(nomes);
+nomes.unshift('Calmon'); // adiciona um item ao inicio do array
+console.log(nomes);
