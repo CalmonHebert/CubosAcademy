e1eeadd8f0d598f8b8c704c568fea1b69a33fb20
CalmonHebert <102420193+CalmonHebert@users.noreply.github.com>
2023-06-28 10:53:33 -0300

    Create Exe05.js

---
+++ 02 - Back-end/Unidade 1/05 - Arrays e Loops/Exercicios/Exe05.js
@@ -0,0 +1,15 @@
+const nomes = ['Calmon', 'Helton', 'João'];
+
+for (let i = 0; i < nomes.length; i++) {
+  if (nomes[i] == 'João') {
+    console.log(`Index: ${i}`);
+  }
+
+}
+
+// for (const i of nomes) {
+//   if (i == 'João'){
+//     console.log(i);
+//   }
+
+// }
