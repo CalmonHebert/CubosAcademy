2dfd0e45825a8bb8986e8a0677516ad304bc2b2e
CalmonHebert <102420193+CalmonHebert@users.noreply.github.com>
2023-06-29 14:32:22 -0300

    Create Ex04 - Múltiplos de 3.js

---
+++ 02 - Back-end/Unidade 1/05 - Arrays e Loops/Exercicios/Classe/Ex04 - Múltiplos de 3.js
@@ -0,0 +1,8 @@
+// Faça um programa que imprima na tela todos os múltiplos de 3 compreendidos entre 0 e 500.
+
+// Os múltiplos de 3 são: 0, 3, 6, 9, 12...
+
+for (let i = 0; i <= 500; i++) {
+  let multiplo = 3 * i;
+  console.log(multiplo);
+}
